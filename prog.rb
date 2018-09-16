0fc1a254e7353acc86954dca2b63ccc22ffa2452
Ndelforno <nicolas.delforno@gmail.com>
2018-09-16 13:28:40 -0400

    strech exercises done

--- prog.rb
+++ prog.rb
@@ -14,9 +14,26 @@ system1.add(planet1)
 system1.add(moon1)
 system1.add(star1)
 #p system1
+
+puts "--------------"
 #system1.add(star1)
 #p star1.class.inspect
 #p system1.bodies
-p Planet.all(system1)
-p Moon.all(system1)
-p Star.all(system1)
+# p Planet.all(system1)
+# p Moon.all(system1)
+# p Star.all(system1)
+system2 = System.new
+planet2 = Planet.new("zorg",3000,10,500)
+moon2 = Moon.new("titan",200,2,planet2)
+moon3 = Moon.new("zen",500,3,planet2)
+star2 = Star.new("ruby",6000,"G-type")
+system2.add(planet2)
+system2.add(moon2)
+system2.add(moon3)
+system2.add(star2)
+#p system2
+p system1.total_mass
+
+
+#p System.systems
+p System.total_mass_all
--- system.rb
+++ system.rb
@@ -1,13 +1,24 @@
 
 class System
+@@total_mass = 0
+@@systems = []
 
   def initialize
     @bodies = []
+    @@systems << self
   end
 
   # readers
   def bodies
     return @bodies
+  end
+
+  def self.total_mass
+   return @@total_mass
+  end
+
+  def self.systems
+    return @@systems
   end
 
   # writers
@@ -23,10 +34,30 @@ class System
     end
   end
 
-
-
-  def total_mass(bodies)
-    bodies(mass).sum
+  def total_mass
+    total_mass = 0
+    bodies.each do |body|
+      total_mass += body.body_mass
+    end
+    return total_mass
   end
+
+  def self.total_mass=()
+   return @@total_mass
+  end
+
+  def self.systems=()
+    return @@systems
+  end
+
+
+  # class methods
+  def self.total_mass_all
+    systems.each do |system|
+      @@total_mass += system.total_mass
+    end
+    return @@total_mass
+  end
+
 
 end
